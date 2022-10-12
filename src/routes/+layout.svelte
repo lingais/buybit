2684a4d37ce154419c991d566067673169e63d44
lingais <lingaiswaran@gmail.com>
2022-10-12 16:42:59 +0800

    First commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+</nav>
+
+
+
+<slot></slot>
+
+<footer>
+    This is a footer
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+
+import {onMount} from "svelte";
+import ky from 'ky';
+import metagrabber from "meta-grabber"
+
+
+let data;
+onMount(async ()=>{
+    const meta = metagrabber
+
+    // const json = await ky.post('https://preview-grabber.vercel.app/api?url=https://www.youtube.com/watch?v=n48j6Yc2zWE&t=27s', {json: {foo: true}}).json();
+
+    // console.log(json);
+    console.log(meta("https://www.youtube.com/watch?v=EQy-AYhZIlE&t=370s"))
+
+})
+
+</script>
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
